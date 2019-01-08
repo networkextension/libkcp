8d07f287d8754eebaf3b2d61b43879b7f34e1f1a
yarshure <yarshure@i9.local>
2019-01-08 14:55:06 +0800

    add iOS target

---
+++ iOSLib/libkcp/KCPiOS/KCPiOS.h
@@ -0,0 +1,19 @@
+//
+//  KCPiOS.h
+//  KCPiOS
+//
+//  Created by yarshure on 8/1/2019.
+//  Copyright © 2019 Kong XiangBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KCPiOS.
+FOUNDATION_EXPORT double KCPiOSVersionNumber;
+
+//! Project version string for KCPiOS.
+FOUNDATION_EXPORT const unsigned char KCPiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KCPiOS/PublicHeader.h>
+
+
